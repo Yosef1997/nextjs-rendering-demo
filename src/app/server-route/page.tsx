c57895a1e7862820bd7a754b5b368bc14ec62ecc
JanBu <yosefsitumorang97@gmail.com>
2024-04-16 11:00:10 +0700

    feat(main):implement client only package

--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,9 +1,11 @@
-import { ImageSlider } from '@/components/imageSlider'
+import { ImageSlider } from '@/components/ImageSlider'
+import { clientSideFunction } from '@/utils/client-utils'
 import { serverSideFunction } from '@/utils/server-utils'
 
 export default function ServerRoute() {
   console.log('Server route rendered')
   const result = serverSideFunction()
+  const clientResult = clientSideFunction()
   return (
     <>
       <h1>Server Route</h1>
